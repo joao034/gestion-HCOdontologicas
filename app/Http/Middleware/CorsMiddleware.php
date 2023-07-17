24b627270eee14fb0a3e9bae95912e5b409b32c5
joao034 <84355621+joao034@users.noreply.github.com>
2023-07-16 19:07:52 -0500

    Cors Middleware

---
+++ app/Http/Middleware/CorsMiddleware.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CorsMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $allowedOrigins = ["https://www.saudemedicalgroup.site"];
+        $origin = $request->header('Origin');
+
+        if (in_array($origin, $allowedOrigins)) {
+            return $next($request);
+            header('Access-Control-Allow-Origin', $origin);
+            header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+            header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+        }
+
+        return $next($request);
+    }
+}

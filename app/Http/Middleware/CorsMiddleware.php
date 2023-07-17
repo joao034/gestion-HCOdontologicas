c9b07ccff1f08b4fd3ede93d570e5cbb050d954e
joao034 <84355621+joao034@users.noreply.github.com>
2023-07-16 19:22:09 -0500

    cambio en CorsMiddleware

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -15,16 +15,19 @@ class CorsMiddleware
      */
     public function handle(Request $request, Closure $next): Response
     {
-        $allowedOrigins = ['https://gestion-hcodontologicas-production.up.railway.app'];
-        $origin = $request->header('Origin');
+        //$allowedOrigins = ['https://www.saudemedicalgroup.site'];
+        //$origin = $request->header('Origin');
 
-        if (in_array($origin, $allowedOrigins)) {
-            return $next($request);
-            header('Access-Control-Allow-Origin', $origin);
-            header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-            header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-        }
-
-        return $next($request);
+        $response = $next($request);
+        $response->headers->set('Access-Control-Allow-Origin', '*');
+        $response->headers->set(
+            'Access-Control-Allow-Methods',
+            'GET, POST, PUT, DELETE, OPTIONS'
+        );
+        $response->headers->set(
+            'Access-Control-Allow-Headers',
+            'Content-Type, X-Auth-Token, Origin, Authorization'
+        );
+        return $response;
     }
 }

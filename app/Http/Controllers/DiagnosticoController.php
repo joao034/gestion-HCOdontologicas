086d92e891375db234471d1cb09594862f7fa71c
Joao <joaojacomedev@gmail.com>
2024-04-10 22:19:04 -0500

    refactor on DiagnosticoController

--- app/Http/Controllers/DiagnosticoController.php
+++ app/Http/Controllers/DiagnosticoController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\DiagnosticoRequest;
 use Illuminate\Http\Request;
 use App\Models\Paciente;
 use App\Models\Diagnostico;
@@ -12,43 +13,28 @@ use App\Models\User;
 
 class DiagnosticoController extends Controller
 {
-
-    private function validate_diagnostico(Request $request){
-        $request->validate([
-            'paciente_id' => 'required',
-            'diagnostico' => 'required',
-            'CIE' => 'required',
-            'tipo' => 'required'
-        ]);
-    }
-
     public function show(int $id_paciente){
         $paciente = Paciente::find($id_paciente);
         $odontologos = User::get_odontologos_activos();
         return view('diagnosticos.show', compact('paciente', 'odontologos'));
     }
 
-    public function edit(int $id_paciente){
-        $paciente = Paciente::find($id_paciente);
+    public function edit(Paciente $paciente){
         return view('diagnosticos.edit', compact('paciente'));
     }
 
-    public function store(Request $request){
+    public function store(DiagnosticoRequest $request){
         try{
-            $this->validate_diagnostico($request);
-            $diagnostico = new Diagnostico();
-            $this->asignar_variables_diagnostico($diagnostico, $request);
+            Diagnostico::create($request->all());
             return to_route('diagnosticos.show', $request->paciente_id)->with('message', 'Diagnóstico guardado correctamente');
         }catch(Exception $e){
             return redirect()->back()->with('danger', 'Error al guardar el diagnóstico. '. $e->getMessage());
         }
     }
 
-    public function update(Request $request, int $id_diagnostico){
+    public function update(DiagnosticoRequest $request, Diagnostico $diagnostico){
         try{
-            $this->validate_diagnostico($request);
-            $diagnostico = Diagnostico::find($id_diagnostico);
-            $this->asignar_variables_diagnostico($diagnostico, $request);
+            $diagnostico->update($request->all());
             return back()->with('message', 'Diagnóstico actualizado correctamente');
         }catch(Exception $e){
             return back()->with('danger', 'Error al actualizar el diagnóstico. '. $e->getMessage());
@@ -64,13 +50,4 @@ class DiagnosticoController extends Controller
             return back()->with('danger', 'Error al eliminar el diagnóstico'. $e->getMessage());
         }
     }
-
-    private function asignar_variables_diagnostico(Diagnostico $diagnostico, Request $request){
-        $diagnostico->paciente_id = $request->paciente_id;
-        $diagnostico->diagnostico = $request->diagnostico;
-        $diagnostico->CIE = $request->CIE;
-        $diagnostico->tipo = $request->tipo;
-        $diagnostico->save();
-    }
-
 }

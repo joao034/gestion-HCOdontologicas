bbaaaf98193c51157ea170e4c55312af000831b6
Joao <joaojacomedev@gmail.com>
2024-04-15 23:53:39 -0500

    fix presupuesto pdf

--- app/Http/Controllers/PresupuestoController.php
+++ app/Http/Controllers/PresupuestoController.php
@@ -24,29 +24,17 @@ class PresupuestoController extends Controller
     public function pdf($presupuesto_id)
     {
         $presupuesto = Odontograma::find($presupuesto_id);
-        $paciente = Paciente::find($presupuesto->paciente_id);
+        $hClinica = HistoriaClinica::find($presupuesto->hclinica_id);
+        $paciente = Paciente::find($hClinica->paciente_id);
         $detalles_presupuesto = $this->getDetallesPresupuesto($presupuesto_id);
         $total_abonado = $this->getTotalAbonado($presupuesto_id);
         $total_realizado = $this->getTotalRealizado($presupuesto_id);
         $pdf = Pdf::loadView('presupuestos.pdf', compact('paciente', 'presupuesto', 'detalles_presupuesto', 'total_abonado', 'total_realizado'));
-        return $pdf->stream('presupuesto_' . $paciente->nombres . ' ' . $paciente->apellidos . '.pdf');
+        return $pdf->stream('presupuesto_' . $paciente->nombreCompleto() . '.pdf');
     }
 
-    public function show(int $paciente_id)
-    {
-        $paciente = Paciente::find($paciente_id);
-
-        // Ordenar los presupuestos por fecha en orden descendente
-        $presupuestos = $paciente->odontogramasCabecera()->latest('updated_at')->get();
-
-        //si el paciente tiene mas de un presupuesto muestra la vista show
-        if ($presupuestos->count() > 1) {
-            return view('presupuestos.show', compact(['paciente', 'presupuestos']));
-        }
-
-        //Si tiene solo un presupuesto redirije directamente al unico presupuesto disponible
-        return to_route('presupuestos.edit', $presupuestos->first()->id);
-    }
+  
+    public function show(int $hclinica_id) {}
 
     private function obtenerIdDelOdontologoEnSesion()
     {
@@ -78,7 +66,6 @@ class PresupuestoController extends Controller
             $detalle_presupuesto->estado = 'necesario';
             $detalle_presupuesto->save();
 
-            DetallePresupuestoModificado::dispatch( $detalle_presupuesto, 'add' );
             return back()->with('message', 'Tratamiento agregado al presupuesto.');
         } catch (Exception $e) {
             return back()->with('danger', 'No se agregó el tratamiento al presupuesto.' . $e->getMessage());
@@ -180,13 +167,6 @@ class PresupuestoController extends Controller
 
     private function getDetallesPresupuesto(int $id)
     {
-        /* $detalles_presupuesto = OdontogramaDetalle::query()
-                                                ->where('odontograma_cabecera_id', '=', "$id")
-                                                ->where( function( $query ) {
-                                                    $query->where('estado', '=', 'necesario')
-                                                    ->orWhere('estado', '=', 'presupuesto')
-                                                    ->orWhere('estado', '=', 'realizado');
-                                                })->get();  */
         $detalles_presupuesto = OdontogramaDetalle::query()
             ->where('odontograma_cabecera_id', '=', "$id")
             ->where('estado', '!=', 'hallazgo')
